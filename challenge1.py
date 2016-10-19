0f5c095085b8b1a46f43e59a8fa3b85557df42c1
Christopher Geiger <christophergeiger3@gmail.com>
2016-10-19 10:11:06 -0400

    Create a program to calculate the area of a circle

---
+++ challenge1.py
@@ -0,0 +1,10 @@
+#Challenge:
+# Make a program that calculates the area of a circle given a radius
+print('Please enter a radius')
+radius = input()
+radius = int(radius)
+area = 3.14 * radius * radius
+print('The area of the circle is ' + str(area))
+
+# Another way to do exponents is **
+# Example: area = 3.14 * radius**2
