44258a5290954ade8ca2cdbe5f2e987380785dda
Christopher Geiger <christophergeiger3@gmail.com>
2017-02-27 15:53:48 -0400

    Create function example solution

---
+++ bipBoopBopSolution.py
@@ -0,0 +1,14 @@
+# Arrange the functions so it prints a story!
+
+def bip():
+    print('who I played fetch with every day')
+
+def boop():
+    print(' named Blop ')
+
+def bop():
+    print('I once had a dog')
+    
+    bop()
+    boop()
+    bip()
