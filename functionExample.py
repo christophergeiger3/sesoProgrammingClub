c1ba6490ca633de200f2de38d7089ad957c0b3bf
Christopher Geiger <christophergeiger3@gmail.com>
2017-02-27 15:54:39 -0400

    Create functionExample.py

---
+++ functionExample.py
@@ -0,0 +1,11 @@
+def addTwoNum(x,y):
+# Function adds two numbers
+# Accepts two parameters - integers
+# Adds them together and stores that value in z
+# Prints z
+    z = x+y
+    print(str(z))
+
+
+
+addTwoNum( 10, 5 )
